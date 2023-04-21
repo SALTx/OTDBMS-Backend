49ee5492d483dc2fa7bf7dcb2e169e0bbe0326be
Saahil <29069313+SALTx@users.noreply.github.com>
2023-04-21 13:34:52 +0800

    Added jquery

--- partials/imports.php
+++ partials/imports.php
@@ -1,6 +1,7 @@
 <!-- Import scripts -->
 <script src="js/fontawesome.js" crossorigin="anonymous"></script>
 <script src="js/chart.js"></script>
+<script src="js/jquery-3.6.4.min.js"></script>
 
 <!-- import css -->
 <link rel="stylesheet" href="css/style.css">
