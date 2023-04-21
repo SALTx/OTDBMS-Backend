f6d0999d2d1dc7af4d3cbdf750ae190f7aff4d26
Saahil <29069313+SALTx@users.noreply.github.com>
2023-04-21 08:53:46 +0800

    light restructure

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overseas Travel Database prototype</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <?php include 'partials/imports.php'; ?>
 </head>
 <body>
