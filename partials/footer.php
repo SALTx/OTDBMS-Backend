2dc667ec5eacc7074bfec828032007ca1f8f56bf
Saahil <29069313+SALTx@users.noreply.github.com>
2023-04-23 00:37:45 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,14 @@
-<footer>
-    ©<?php echo date('Y'); ?> OITP
+<footer class="text-center text-white" style="background-color: #f1f1f1;">
+    <div class="container pt-4">
+        <section class="mb-1">
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="mailto:saahilthadani@gmail.com" role="button" data-mdb-ripple-color="dark"><i class="fas fa-envelope"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/saahil-thadani-48a983178/" role="button" data-mdb-ripple-color="dark"><i class="fab fa-linkedin"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/saltx" role="button" data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
+        </section>
+    </div>
+
+    <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © <?php echo date('Y'); ?> OTDBMS
+        <p>Made with ❤️ by <a href="https://github.com/saltx">Saahil Thadani</a></p>
+    </div>
 </footer>
