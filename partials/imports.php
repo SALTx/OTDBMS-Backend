75f72cf240ae208d9316381b694582c24b587403
Saahil <29069313+SALTx@users.noreply.github.com>
2023-04-23 00:32:15 +0800

    Added bootstrap

--- partials/imports.php
+++ partials/imports.php
@@ -2,7 +2,9 @@
 <script src="js/fontawesome.js" crossorigin="anonymous"></script>
 <script src="js/chart.js"></script>
 <script src="js/jquery-3.6.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 
 <!-- import css -->
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/table.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
