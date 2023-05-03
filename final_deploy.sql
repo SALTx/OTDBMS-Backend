b2d74eda7b54cdc933fef593ef11c3babdea8562
191203G <94876843+191203G@users.noreply.github.com>
2023-05-03 08:54:30 +0800

    Update final_deploy.sql

--- final_deploy.sql
+++ final_deploy.sql
@@ -10,42 +10,45 @@ CREATE TABLE IF NOT EXISTS students (
     name VARCHAR(64) NOT NULL,
     gender ENUM('Male', 'Female') NOT NULL,
     birthday DATE NOT NULL,
-    citizenshipStatus VARCHAR(64)，
-    diploma VARCHAR(64),
-    pemGroup VARCHAR(64),
+    citizenshipStatus ENUM('Singapore citizen', 'Permanent resident', 'Foreigner') NOT NULL,
+    countryOfOrigin CHAR (3),
+    course CHAR(3),
+    year tiny int NOT NULL,
+    pemGroup CHAR(6),
     FOREIGN KEY (pemName) REFERENCES users(name)
-    FOREIGN KEY (diploma) REFERENCES diplomas(diplomaName)
+    FOREIGN KEY (course) REFERENCES course(courseName)
+    FOREIGN KEY (countryOfOrigin) REFERENCES countries(countryCode)
 );
 -- create table for Diploma accounts
-CREATE TABLE IF NOT EXISTS diplomas (
-    diplomaId VARCHAR(64) NOT NULL PRIMARY KEY,
-    diplomaName VARCHAR(64),
+CREATE TABLE IF NOT EXISTS course (
+    courseCode CHAR(3) NOT NULL PRIMARY KEY,
+    courseName VARCHAR(64),
+    courseManager VARCHAR(64),
 );
 
 -- create overseasProgrammes table
 CREATE TABLE IF NOT EXISTS overseasPrograms (
-    programID VARCHAR(64) NOT NULL PRIMARY KEY,
+    programID CHAR(6) NOT NULL PRIMARY KEY,
     programName VARCHAR(64) NOT NULL,
     programType ENUM('Internship', 'Exchange program', 'Immersion program', 'Others') NOT NULL, 
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    country VARCHAR(64) NOT NULL,
+    countryCode CHAR(3) NOT NULL,
     organization VARCHAR(64),
-    organizationType VARCHAR(64),
-    FOREIGN KEY (country) REFERENCES countries(countryName)
+    organizationType ENUM('School','Industrial','Others') NOT NULL,
+    FOREIGN KEY (countryCode) REFERENCES countries(countryCode)
 );
 -- create table countries
 CREATE TABLE IF NOT EXISTS countries (
     countryCode CHAR(3) NOT NULL PRIMARY KEY,
     countryName VARCHAR(64) NOT NULL,
     city VARCHAR(64) NOT NULL,
-    numberOfTrips SMALLINT UNSIGNED,
     aciCountry ENUM('Yes', 'No')
 );
 -- create table trips
 CREATE TABLE IF NOT EXISTS trips (
     studentAdminNumber CHAR(7) NOT NULL,
-    programID VARCHAR(64) NOT NULL,
+    programID CHAR(6) NOT NULL,
     comments TEXT,
     PRIMARY KEY (studentAdminNumber, programID),
     FOREIGN KEY (studentAdminNumber) REFERENCES students(adminNumber),
@@ -55,9 +58,9 @@ CREATE TABLE IF NOT EXISTS trips (
 -- create table for user accounts
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(64) NOT NULL PRIMARY KEY,
-    password VARCHAR(64),
+    password VARCHAR(64)  NOT NULL,
     accountType ENUM('Admin', 'Teacher', 'Guest'),
-    name VARCHAR(64)
+    name VARCHAR(64)  NOT NULL,
 );
 
 
