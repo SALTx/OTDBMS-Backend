f1a2f7ffeafcb1915116ea9b2b6cf12a19e17e33
191203G <94876843+191203G@users.noreply.github.com>
2023-05-16 16:35:17 +0800

    Update proce.sql

--- SQL/proce.sql
+++ SQL/proce.sql
@@ -1,23 +1,6 @@
 DELIMITER //
 
-CREATE PROCEDURE AddNewStudent(
-  IN p_adminNo CHAR(7),
-  IN p_name VARCHAR(64),
-  IN p_gender ENUM('Male', 'Female'),
-  IN p_citizenshipStatus ENUM('Singapore citizen', 'Permanent resident', 'Foreigner'),
-  IN p_course CHAR(3),
-  IN p_stage TINYINT,
-  IN p_pemGroup CHAR(6)
-)
-BEGIN
-  INSERT INTO students (adminNo, name, gender, citizenshipStatus, course, stage, pemGroup)
-  VALUES (p_adminNo, p_name, p_gender, p_citizenshipStatus, p_course, p_stage, p_pemGroup);
-END //
-
-DELIMITER ;
-
-DELIMITER //
-
+-- Stored procedure to update the date in the overseasprogramm table
 CREATE PROCEDURE UpdateProgramDates(
   IN p_programID CHAR(6),
   IN p_startDate DATE,
@@ -28,5 +11,31 @@ BEGIN
   SET startDate = p_startDate, endDate = p_endDate
   WHERE programID = p_programID;
 END //
+
+--CALL UpdateProgramDates('OP001', '2023-06-01', '2023-08-31');
+-- Stored procedure to update the comments in the trips table
+
+CREATE PROCEDURE UpdateTripComments(
+  IN p_studentAdminNo CHAR(7),
+  IN p_programID CHAR(6),
+  IN p_comments TEXT
+)
+BEGIN
+    UPDATE trips SET comments = p_comments WHERE studentAdminNo = p_studentAdminNo AND programID = p_programID;
+END //
+
+--CALL UpdateTripComments('A123456', 'OP001', 'New comment');
+
+CREATE PROCEDURE InsertCountry(
+  IN p_countryCode CHAR(3),
+  IN p_countryName VARCHAR(64),
+  IN p_aciCountry ENUM('Yes', 'No')
+)
+BEGIN
+  INSERT INTO countries (countryCode, countryName, aciCountry)
+  VALUES (p_countryCode, p_countryName, p_aciCountry);
+END //
+
+--CALL InsertCountry('USA', 'United States', 'No');
 
 DELIMITER ;
