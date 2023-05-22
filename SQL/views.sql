c789748c2491823146cb1249ac09d8ee34f3d8e6
191203G <94876843+191203G@users.noreply.github.com>
2023-05-22 16:25:33 +0800

    views

--- SQL/views.sql
+++ SQL/views.sql
@@ -15,3 +15,20 @@ FROM trips t
 JOIN overseasPrograms op ON t.programID = op.programID
 JOIN countries c ON op.countryCode = c.countryCode
 WHERE c.aciCountry = 'Yes' AND op.programType = 'OITP';
+
+CREATE VIEW OIMPdetailsView AS
+SELECT 
+    t.studAdmin, 
+    t.programID, 
+    op.programName,
+    op.city,
+    op.partnerName,
+    s.course,
+    od.gsmCode,
+    od.courseCode,
+    od.gsmName
+FROM trips t
+JOIN overseasPrograms op ON t.programID = op.programID
+JOIN students s ON t.studAdmin = s.adminNo
+JOIN OIMPdetails od ON t.studAdmin = od.studAdmin AND t.programID = od.programID;
+
