eee0b4d737cda9a6b25784fca28951d747323662
191203G <94876843+191203G@users.noreply.github.com>
2023-05-24 12:48:27 +0800

    auto increment programID and python for auto generating data

---
+++ SQL/table.sql
@@ -0,0 +1,88 @@
+
+CREATE DATABASE IF NOT EXISTS overseasProgramDB;
+
+USE overseasProgramDB;
+
+CREATE TABLE IF NOT EXISTS countries (
+    countryCode char(2),
+    countryName varchar(64),
+    aciCountry enum ('A', 'N'),
+    PRIMARY KEY (countryCode)
+);
+
+CREATE TABLE IF NOT EXISTS pemGroup (
+    pemGroupId char(6) not null,
+    pemName varchar(64),
+    PRIMARY KEY (pemGroupId)
+);
+
+CREATE TABLE IF NOT EXISTS course (
+    courseCode char(6) not null,
+    courseName varchar(64),
+    courseManager varchar(64),
+    PRIMARY KEY (courseCode)
+);
+
+-- Edge case: Common programs which branch out to different courses
+
+CREATE TABLE IF NOT EXISTS students (
+    adminNo char(7) not null,
+    #--adminNo format: first 2 digit as enroll year,middle 4 random digit, last 1 random alphabet. 
+    #--e.g. 191203G. 19 as enrolled in year 2019,the rest is all random generated
+    name varchar(64) not null,
+    gender enum ('Male', 'Female') not null,
+    citizenshipStatus enum ('Singapore citizen', 'Permanent resident', 'International Student') not null,
+    -- consider grouping singaporean and pr together for certain views
+    stage tinyint not null,
+    -- stage 1,2,3 is like year 1,2,3. the max number for stage is 3
+    course char(6) not null,
+    pemGroup char(6) not null,
+    PRIMARY KEY (adminNo),
+    FOREIGN KEY (course) REFERENCES course (courseCode),
+    FOREIGN KEY (pemGroup) REFERENCES pemGroup (pemGroupId)
+    );
+CREATE TABLE IF NOT EXISTS overseasPrograms (
+    programID char(9) NOT NULL,
+    programName varchar(64),
+    programType ENUM
+    ('Overseas educational trip', 'Overseas internship program', 'Overseas immersion program', 
+    'Overseas Competition/Exchange', 'Overseas Leadership Training', 'Overseas Leadership Training with Outward Bound',
+    'Overseas Service Learning-Youth Expedition Programme'),
+    startDate date,
+    endDate date,
+    countryCode char(2),
+    city varchar(64),
+    partnerName varchar(64),
+    overseasPartnerType enum ('Company', 'Institution', 'Others'),
+    PRIMARY KEY (programID, countryCode, city)
+);
+
+CREATE TABLE IF NOT EXISTS OIMPdetails (
+    gsmCode varchar(20) not null,
+    courseCode char(6) not null,
+    studAdmin char(7) not null,
+    gsmName varchar(50) not null,
+    programID char(9) not null,
+    PRIMARY KEY (gsmCode),
+    FOREIGN KEY (courseCode) REFERENCES course (courseCode),
+    FOREIGN KEY (studAdmin) REFERENCES students (adminNo)
+);
+
+-- Edge case: Trips that include multiple destinations
+CREATE TABLE IF NOT EXISTS trips (
+    studAdmin char(7) not null,
+    programID char(9) not null,
+    comments text,
+    PRIMARY KEY (studAdmin, programID),
+    FOREIGN KEY (studAdmin) REFERENCES students (adminNo),
+    FOREIGN KEY (programID) REFERENCES overseasPrograms (programID)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    -- not fully implemented
+    username varchar(64) not null,
+    password varchar(64),
+    accountType enum ('Admin', 'Teacher', 'Guest'),
+    name varchar(64),
+    PRIMARY KEY (username)
+);
---
+++ SQL/views&proce.sql
@@ -0,0 +1,153 @@
+
+CREATE VIEW KPI1OverseasStudCount AS
+SELECT COUNT(DISTINCT studAdmin) AS StudentCount
+FROM trips;
+
+CREATE VIEW KPI2ACIcount AS
+SELECT COUNT(DISTINCT t.studAdmin) AS StudentCount
+FROM trips t
+JOIN overseasPrograms op ON t.programID = op.programID
+JOIN countries c ON op.countryCode = c.countryCode
+WHERE c.aciCountry = 'A';
+
+CREATE VIEW KPI3ACIoitp AS
+SELECT COUNT(DISTINCT t.studAdmin) AS StudentCount
+FROM trips t
+JOIN overseasPrograms op ON t.programID = op.programID
+JOIN countries c ON op.countryCode = c.countryCode
+WHERE c.aciCountry = 'A' AND op.programType = 'Overseas internship program';
+
+CREATE VIEW OIMPdetailsView AS
+SELECT 
+    t.studAdmin, 
+    t.programID, 
+    op.programName,
+    op.city,
+    op.partnerName,
+    s.course,
+    od.gsmCode,
+    od.courseCode,
+    od.gsmName
+FROM trips t
+JOIN overseasPrograms op ON t.programID = op.programID
+JOIN students s ON t.studAdmin = s.adminNo
+JOIN OIMPdetails od ON t.studAdmin = od.studAdmin AND t.programID = od.programID;
+
+DELIMITER //
+CREATE PROCEDURE getProgramAcronym(programType VARCHAR(64), OUT acronym CHAR(3))
+BEGIN
+    CASE programType
+    WHEN 'Overseas educational trip' THEN SET acronym = 'OET';
+    WHEN 'Overseas internship program' THEN SET acronym = 'OIP';
+    WHEN 'Overseas immersion program' THEN SET acronym = 'IMP';
+    WHEN 'Overseas Competition/Exchange' THEN SET acronym = 'OCP';
+    WHEN 'Overseas Leadership Training' THEN SET acronym = 'OLT';
+    WHEN 'Overseas Leadership Training with Outward Bound' THEN SET acronym = 'TOB';
+    WHEN 'Overseas Service Learning-Youth Expedition Programme' THEN SET acronym = 'YEP';
+    ELSE SET acronym = '';
+    END CASE;
+END//
+DELIMITER //
+-- programID format 
+-- programType in shortcut as 1st,2nd,3rd,position
+-- Overseas educational trip (OET),
+-- Overseas internship program (OIP), 
+-- Overseas immersion program (IMP), 
+-- Overseas Competition/Exchange (OCP), 
+-- Overseas Leadership Training (OLT), 
+-- Overseas Leadership Training with Outward Bound (TOB),
+-- Overseas Service Learning-Youth Expedition Programme (YEP)
+
+-- Year as 4th and 5th position
+
+-- ACI or NON-ACI as 6th position
+-- N = NON-ACI
+-- A = ACI
+
+-- 000-999 as sequence of program that happened in the year as 7th,8th,9th position
+-- e.g., OET23A001 = Overseas educational trip in ACI country ,this program is the 1st program of all overseas programs in year 2023
+-- IMP23N002 = Overseas immersion program in NON-ACI country ,this program is the 2nd program of all overseas programs in year 2023
+
+DELIMITER //
+CREATE TRIGGER programID_Before_Insert
+BEFORE INSERT
+ON overseasPrograms FOR EACH ROW
+BEGIN
+    DECLARE acronym CHAR(3);
+    DECLARE year CHAR(4);
+    DECLARE aciChar CHAR(1);
+    DECLARE seqNum CHAR(3); -- change this line
+    DECLARE newProgramID CHAR(9);
+
+    -- Call the stored procedure to get the acronym
+    CALL getProgramAcronym(NEW.programType, acronym);
+
+    -- Get the year from the startDate
+    SET year = SUBSTRING(YEAR(NEW.startDate), 3, 2);
+
+    -- Get the ACI or NON-ACI character directly from the countries table
+    SET aciChar = (SELECT aciCountry FROM countries WHERE countryCode = NEW.countryCode);
+
+    -- Get the next sequence number
+    SET seqNum = LPAD((SELECT COUNT(*) + 1 FROM overseasPrograms WHERE SUBSTRING(programID, 4, 2) = year), 3, '0');
+
+    -- Construct the new programID
+    SET newProgramID = CONCAT(acronym, year, aciChar, seqNum);
+
+    -- Set the new programID
+    SET NEW.programID = newProgramID;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE DeleteCountry(IN p_countryCode CHAR(2))
+BEGIN
+    DELETE FROM countries WHERE countryCode = p_countryCode;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateStudentStage(IN p_adminNo CHAR(7), IN p_stage TINYINT)
+BEGIN
+    UPDATE students SET stage = p_stage WHERE adminNo = p_adminNo;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE updateCitizenship(IN p_adminNo CHAR(7), IN p_citizenshipStatus ENUM('Singapore citizen', 'Permanent resident', 'Foreigner'))
+BEGIN
+    UPDATE students SET citizenshipStatus = p_citizenshipStatus WHERE adminNo = p_adminNo;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateStudentCourse(IN p_adminNo CHAR(7), IN p_course CHAR(3))
+BEGIN
+    UPDATE students SET course = p_course WHERE adminNo = p_adminNo;
+END //
+DELIMITER 
+
+DELIMITER //
+CREATE PROCEDURE DeletePemGroup(IN p_pemGroupId CHAR(6))
+BEGIN
+    DELETE FROM pemGroup WHERE pemGroupId = p_pemGroupId;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteCourse(IN p_courseCode CHAR(3))
+BEGIN
+    DELETE FROM course WHERE courseCode = p_courseCode;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE UpdateCourseManager(IN p_courseCode CHAR(3), IN p_courseManager VARCHAR(64))
+BEGIN
+    UPDATE course SET courseManager = p_courseManager WHERE courseCode = p_courseCode;
+END //
+DELIMITER ;
+
+
